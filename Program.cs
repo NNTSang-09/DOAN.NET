e1b7f9307e8fa46944f45fb7e6c0d0abc57ddce0
Phucvhcold04 <toitenphuc12@gmail.com>
2024-12-14 15:51:46 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quan_ly_Ban_Thuoc
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            string connectionString = @"Data Source=LAPTOP-H0J8DGVP\SQLEXPRESS;Initial Catalog=pharmacy;Integrated Security=True;TrustServerCertificate=True;";
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmLogin(connectionString));
+        }
+    }
+}
